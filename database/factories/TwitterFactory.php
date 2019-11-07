33612be7e024c628f53967fc091bafb9b722e30d
Marcel Oliveira <m.rabello23@gmail.com>
2019-11-06 22:05:31 -0300

    criado estrutura dp app

---
+++ database/factories/TwitterFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Twitter;
+use Faker\Generator as Faker;
+
+$factory->define(Twitter::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
